ab46acc63d0581b5632e6ab6202730cb8b1d686e
unani <unani92@naver.com>
2020-06-26 23:27:13 +0900

    fin infinite scroll

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,38 +7,41 @@ import Movie from "./Movie"
 
 function Home({ state, dispatch }) {
   const [isLoading,setIsLoading] = useState(true)
-  const [isScroll, setScroll] = useState(0)
+  const [isScroll, setScroll] = useState(1)
 
   function myaxios() {
-    setScroll(isScroll+1)
+    console.log(isScroll)
     axios.get(`https://finprojectapi.herokuapp.com/api/v1/community/movies/?page=${isScroll}`)
       .then(res => {
         dispatch(addMovies(res.data))
-        setIsLoading(false)
-        console.log(state)
+        if (isLoading) {setIsLoading(false)}
       })
       .catch(err => console.log(err))
   }
-  // function addScroll() {
-  //   const bottomSensor = document.querySelector("#bottomSensor")
-  //   const watcher = scrollMonitor.create(bottomSensor)
-  //   watcher.enterViewport(() => myaxios())
-  // }
-  // useEffect(addScroll)
   useEffect(myaxios,[isScroll])
+
+  if (!isLoading) {
+    setTimeout(() => {
+      const bottomSensor = document.getElementById("bottomSensor")
+      const watcher = scrollMonitor.create(bottomSensor)
+      watcher.enterViewport(() => setTimeout(
+        () => setScroll(isScroll+1)
+      ),500)
+    },500)
+  }
+
   return (
     <div className="container">
-      <div className="row">
-        {isLoading ? "loading" :
-          state.map(movie => (
-            <div className="col-4">
-              <Movie key={Date.now()} id={movie.id} poster_path={movie.poster_path} title={movie.title}/>
+      {isLoading ? "is Loading" :
+        (<div className="row">
+          {state.map((movie,index) => (
+            <div key={index} className="col-4">
+              <Movie key={index} id={movie.id} poster_path={movie.poster_path} title={movie.title}/>
             </div>
-            )
-          )
-        }
-      </div>
-      <div id="bottomSensor"></div>
+          ))}
+          <div id="bottomSensor"></div>
+        </div>)
+      }
     </div>
   )
 }
